efcb4a2bc59ef2cf6a65a8b9cc5c2c99939e2ff2
Yona <3211998+yonacwy@users.noreply.github.com.>
2025-03-14 05:38:26 -0400

    added responsive

--- packages/perf-react/src/App.tsx
+++ packages/perf-react/src/App.tsx
@@ -1,10 +1,16 @@
+import { useMediaQuery } from "react-responsive";
 import Editor from "./app/Editor";
 import "shared/styles/perf-app.css";
 import "shared/styles/nodes-menu.css";
 
 function App() {
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+
   return (
-    <div className="editors">
+    <div
+      className={`editors ${isDesktop ? "desktop" : "mobile"}`}
+      style={{ overflowX: "auto", display: "flex", flexDirection: "column" }}
+    >
       <div className="editor">
         <Editor
           {...{
@@ -16,6 +22,32 @@ function App() {
           }}
         />
       </div>
+      {!isDesktop && (
+        <div className="editor">
+          <Editor
+            {...{
+              serverName: "dbl",
+              organizationId: "bfbs",
+              languageCode: "eng",
+              versionId: "web",
+              bookCode: "tit",
+            }}
+          />
+        </div>
+      )}
+      {isDesktop && (
+        <div className="editor">
+          <Editor
+            {...{
+              serverName: "dbl",
+              organizationId: "bfbs",
+              languageCode: "eng",
+              versionId: "web",
+              bookCode: "tit",
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 }
